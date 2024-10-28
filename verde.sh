2c8d8f26e4eeaa7c8e61569a6350b12e6020d559
André Luís <andreluis2442@gmail.com>
2024-10-28 15:48:23 -0300

    Improve menu and add multiple test case
    - Add support for multiple test cases.
    - Improve menu to include TP Builder.
    - Improve flags support.
    - Refactor code to improve readability.

--- verde.sh
+++ verde.sh
@@ -1,159 +1,231 @@
 #!/bin/bash -eu
 
-# Script para correção automática de exercícios de programação
-# Recebe parametros:
-#   -t : flag para teste automático (pub.in e pub.out necessários)
-
 DIR=$(readlink $(which verde) | xargs dirname)
-SUP_LANGS=(java c cpp)
-CODE_FILES=()
+TP_BUILDER_DIR="$DIR/scripts/tp-builder.sh"
+SUPPORTED_LANGUAGES=("c" "cpp" "java")
 CODE_FILE=""
-CODE_LANG=""
-TEST_FLAG=0
-COMPILE_FLAG=0
+CODE_LANGUAGE=""
+TEST_CASES=()
 
-# Processar as flags
-while getopts 'ct' opt; do
-  case $opt in
-    c) COMPILE_FLAG=1 ;;
-    t) TEST_FLAG=1 ;;
-    *) printf "Uso: %s [-c -t]\n" "$0" 1>&2; exit 1 ;;
+# get source code file
+get_source_code() {
+  local code_files=()
+
+  for language in "${SUPPORTED_LANGUAGES[@]}"; do
+    for file in *."$language"; do
+      if [ -f "$file" ]; then
+        code_files+=("$file")
+      fi
+    done
+  done
+
+  if [ ${#code_files[@]} -eq 0 ]; then
+    echo "Nenhum arquivo de código-fonte encontrado" >&2
+    exit 1
+  elif [ ${#code_files[@]} -eq 1 ]; then
+    CODE_FILE="${code_files[0]}"
+  else
+    PS3="Selecione um arquivo de código-fonte (1-${#code_files[@]}): "
+    select file in "${code_files[@]}"; do
+      if [ -n "$file" ]; then
+        CODE_FILE="$file"
+        break
+      else
+        echo "Opção inválida. Por favor, tente novamente." >&2
+      fi
+    done
+  fi
+
+  CODE_LANGUAGE="${CODE_FILE##*.}"
+}
+
+# get test cases files (.in and .out)
+get_test_cases() {
+  for file in *.in; do
+    if [ -f "$file" -a -f "${file%.*}.out" ]; then
+      TEST_CASES+=("${file%.*}")
+    fi
+  done
+
+  if [ ${#TEST_CASES[@]} -eq 0 ]; then
+    echo "Nenhum caso de teste encontrado" >&2
+    exit 1
+  fi
+}
+
+# compare two files and show differences
+get_files_diff() {
+  local file1=$1
+  local file2=$2
+
+  # get number of lines of each file
+  local lines_arq1=$(wc -l < "$file1")
+  local lines_arq2=$(wc -l < "$file2")
+
+  # get number of different lines
+  local diff_lines=$(git diff --ignore-cr-at-eol --no-index --stat "$file1" "$file2" | tail -1 | awk '{print $4}')
+
+  # check if files are equal or different
+  if [ -z "$diff_lines" ]; then
+    echo -e "$(tput setaf 3)$(tput bold)\n Nota: 100% \U0001F389 \n$(tput sgr0)"
+  else
+    # calculate difference percentage
+    local diff_perc=$(awk "BEGIN { printf \"%.2f\", ($diff_lines / ($lines_arq1 + $lines_arq2)) * 100 }")
+
+    # calculate equality percentage
+    local equal_perc=$(awk "BEGIN { printf \"%.2f\", 100 - $diff_perc }")
+
+    # show equality percentage
+    echo -e "$(tput setaf 3)$(tput bold)\n Nota: $equal_perc%$(tput sgr0)\n"
+
+    # show different lines
+    echo -e "$(tput setaf 1)$(tput bold) \U000026A0 Diferenças:$(tput sgr0)"
+
+    git diff --ignore-cr-at-eol --no-index --line-prefix="  " --word-diff=color -U0 --color=always "$file1" "$file2" | tail -n +5
+    echo
+  fi
+}
+
+# compile code
+compile() {
+  case $CODE_LANGUAGE in
+    "java")
+      javac "$CODE_FILE"
+      ;;
+    "c")
+      gcc -o "${CODE_FILE%.*}" "$CODE_FILE"
+      ;;
+    "cpp")
+      g++ -o "${CODE_FILE%.*}" "$CODE_FILE"
+      ;;
+    *)
+      echo "Linguagem não suportada" >&2
+      exit 1
+      ;;
   esac
-done
+}
 
-# Se nenhuma flag foi passada, executar o script grafite.sh
-if [ $OPTIND -eq 1 ]; then
-  # Escolher o que executar
+# execute code
+execute() {
+  case $CODE_LANGUAGE in
+    "java")
+      java "${CODE_FILE%.*}"
+    ;;
+    "c")
+      ./"${CODE_FILE%.*}"
+    ;;
+    "cpp")
+      ./"${CODE_FILE%.*}"
+    ;;
+    *)
+      echo "Linguagem não suportada" >&2
+      exit 1
+      ;;
+  esac
+}
+
+# test code with all test cases
+test_code() {
+  get_test_cases
+
+  for test_case in "${TEST_CASES[@]}"; do
+    case $CODE_LANGUAGE in
+      "java")
+        java "${CODE_FILE%.*}" < "$test_case.in" > "$test_case.res"
+        ;;
+      "c")
+        ./"${CODE_FILE%.*}" < "$test_case.in" > "$test_case.res"
+        ;;
+      "cpp")
+        ./"${CODE_FILE%.*}" < "$test_case.in" > "$test_case.res"
+        ;;
+      *)
+        echo "Linguagem não suportada" >&2
+        exit 1
+        ;;
+    esac
+
+    echo -e "$(tput setaf 6)$(tput bold)> Caso de teste: $(tput smul)$test_case.in$(tput sgr0)"
+
+    get_files_diff "$test_case.res" "$test_case.out"
+  done
+}
+
+# compile and execute code
+compile_and_execute() {
+  get_source_code
+
+  compile
+  execute
+}
+
+# compile, execute and test code
+execute_tests() {
+  get_source_code
+
+  compile
+  test_code
+}
+
+# menu options
+menu() {
+  echo "Bem-vindo ao Verde CLI!"
+
   PS3="Selecione a opção desejada: "
-  select opt in "Executar código" "Corrigir exercício" "TP Builder"; do
+  select opt in "Compilar e executar código" "Executar casos de teste" "TP Builder"; do
     case $opt in
       "Executar código")
-        COMPILE_FLAG=1
+        compile_and_execute
         break
         ;;
       "Corrigir exercício")
-        TEST_FLAG=1
+        execute_tests
         break
         ;;
       "TP Builder")
-        bash $DIR/grafite/grafite.sh
+        bash "$TP_BUILDER_DIR"
         exit 0
         ;;
       *)
-        echo "Opção inválida. Por favor, tente novamente." >&2
+        echo "Opção inválida!"
+        exit 1
         ;;
     esac
   done
-fi
+}
 
+# clear screen
+clear -x
 
-for lang in "${SUP_LANGS[@]}"; do
-  # Usar globbing para encontrar arquivos com a extensão atual da iteração
-  for file in *."$lang"; do
-    # Verificar se o arquivo realmente existe (necessário caso não haja arquivos para uma das extensões)
-    if [ -f "$file" ]; then
-      CODE_FILES+=("$file")
-    fi
-  done
+# process flags
+while getopts 'hctb' opt; do
+  case $opt in
+    c)
+      compile_and_execute
+      exit 0
+      ;;
+    t)
+      execute_tests
+      exit 0
+      ;;
+    b)
+      bash "$TP_BUILDER_DIR"
+      exit 0
+      ;;
+    *)
+      echo "Uso: verde [OPÇÃO]"
+      echo "Opções:"
+      echo "  -h  Mostra esta mensagem de ajuda"
+      echo "  -c  Compila e executa seu código"
+      echo "  -t  Executa os casos de teste"
+      echo "  -b  Executa o TP Builder"
+
+      exit 0
+      ;;
+  esac
 done
 
-if [ ${#CODE_FILES[@]} -eq 0 ]; then
-  # Se nenhum arquivo foi encontrado, exibir uma mensagem de erro e sair
-  echo "Nenhum arquivo de código-fonte encontrado" >&2
-  exit 1
-elif [ ${#CODE_FILES[@]} -eq 1 ]; then
-  # Se apenas um arquivo foi encontrado, não é necessário selecionar
-  CODE_FILE="${CODE_FILES[0]}"
-else
-  PS3="Selecione um arquivo de código-fonte (1-${#CODE_FILES[@]}): "
-  select file in "${CODE_FILES[@]}"; do
-    if [ -n "$file" ]; then
-      CODE_FILE="$file"
-      break
-    else
-      echo "Opção inválida. Por favor, tente novamente." >&2
-    fi
-  done
-fi
-
-CODE_LANG="${CODE_FILE##*.}"
-
-
-# Função para ver a diferença de dois arquivos
-filesDiff() {
-  # Contar o número total de linhas em ambos os arquivos
-  lines_arq1=$(wc -l < $1)
-  lines_arq2=$(wc -l < $2)
-
-  # Contar o número de linhas diferentes usando git diff
-  diff_lines=$(git diff --ignore-cr-at-eol --no-index --stat "$1" "$2" | tail -1 | awk '{print $4}')
-
-  # Verificar se diff_lines está definido e não está vazio
-  if [ -z "$diff_lines" ]; then
-    echo -e $(tput setaf 3)$(tput bold)"\n | Nota: 100% \U0001F389 \n" $(tput sgr0)
-    exit 1
-  else
-    # Calcular a porcentagem de diferença
-    perc_diff=$(awk "BEGIN { printf \"%.2f\", ($diff_lines / ($lines_arq1 + $lines_arq2)) * 100 }")
-
-    # Calcular a porcentagem de igualdade
-    perc_igualdade=$(awk "BEGIN { printf \"%.2f\", 100 - $perc_diff }")
-
-    # Exibir a porcentagem de igualdade
-    echo -e $(tput setaf 3)$(tput bold)"\n | Nota: $perc_igualdade%$(tput sgr0)\n"
-
-    # Exibir linhas diferentes
-    echo -e $(tput setaf 4)$(tput bold)" | \U000026A0 Diferenças:\n"$(tput sgr0)
-
-    git diff --ignore-cr-at-eol --no-index --line-prefix="   " --word-diff=color -U0 --color=always "$1" "$2" | tail -n +5
-  fi
-}
-
-# Compilar o arquivo de acordo com a linguagem
-case $CODE_LANG in
-  java)
-    javac "$CODE_FILE"
-    ;;
-  c)
-    gcc -o "${CODE_FILE%.*}" "$CODE_FILE"
-    ;;
-  cpp)
-    g++ -o "${CODE_FILE%.*}" "$CODE_FILE"
-    ;;
-esac
-
-executar_java() {
-  CLASS_FILE=$(basename "$CODE_FILE" .java)
-
-  # Executar o arquivo compilado
-  if [ $TEST_FLAG -eq 1 ]; then
-    if [ -f "pub.in" ] && [ -f "pub.out" ]; then
-      java "${CLASS_FILE}" < "pub.in" > "a.out"
-      filesDiff "a.out" "pub.out"
-    else
-      echo "Arquivos de teste não encontrados" >&2
-    fi
-  else
-    java "${CLASS_FILE}"
-  fi
-}
-
-# Executar o arquivo compilado
-if [ $TEST_FLAG -eq 1 ]; then
-  if [ -f "pub.in" ] && [ -f "pub.out" ]; then
-    if [ $CODE_LANG == "java" ]; then
-      executar_java
-    elif [ $CODE_LANG == "c" ] || [ $CODE_LANG == "cpp" ]; then
-      ./"${CODE_FILE%.*}" < "pub.in" > "a.out"
-      filesDiff "a.out" "pub.out"
-    fi
-  else
-    echo "Arquivos de teste não encontrados" >&2
-  fi
-else
-  if [ $CODE_LANG == "java" ]; then
-    executar_java
-  else
-    ./"${CODE_FILE%.*}"
-  fi
+# show menu if no flag is passed
+if [ $OPTIND -eq 1 ]; then
+  menu
 fi
